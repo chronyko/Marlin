4534b4208da83a9987bc131a9946dd3edde55bb0
Martyn <martyn@chronyko.com>
2020-09-25 13:45:05 +0100

    Updated E-steps after upgrade

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -738,7 +738,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.43, 78.43, 800, 98.41 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.43, 78.43, 800, 96.96 }
 
 /**
  * Default Max Feed Rate (mm/s)
